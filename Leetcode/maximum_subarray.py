05b3d1842a0308c1f996d767b1155b11ee68ef1c
Nihiru <nikhil.m.alors@gmail.com>
2020-04-06 20:26:21 +0530

    Day 2 - Max Subarray

--- Leetcode/maximum_subarray.py
+++ Leetcode/maximum_subarray.py
@@ -7,13 +7,11 @@ Created on Sat Apr  4 17:34:37 2020
 """
 
 def maximum_subarray(array):
-    # setting the maximum sub array
+    # setting the maximum sub array similar to 
     max_sum = array[0]
     current_sum = max_sum
     for ele in array[1:]:
-        current_sum = max(ele + current_sum, ele)
-        max_sum = max(current_sum, max_sum)
+        current_sum = max(ele + current_sum, ele) # adding elements to the existing array and determining the greatest
+        max_sum = max(current_sum, max_sum) # identifying the maximum between maximum_sum and pre-calculated current_sum
     
     return max_sum
-        
-        s
