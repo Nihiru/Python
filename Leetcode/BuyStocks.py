6d2dae48bcb62176e4816fa26b5512cbbdaea468
Nihiru <nikhil.m.alors@gmail.com>
2020-04-07 22:46:37 +0530

    Day 4 - Buy Stocks(comments)

--- Leetcode/BuyStocks.py
+++ Leetcode/BuyStocks.py
@@ -4,15 +4,16 @@
 """
 def maxProfit(PriceList):
     best_without_stock = 0;
-    best_with_stock = -(1e9 +5)
+    best_with_stock = -(1e9 +5) # setting the infinity value
     for x in PriceList:
-        
-        best_with_stock = max(best_with_stock, best_without_stock - x)
-        best_without_stock = max(best_without_stock, best_with_stock +x)
-        print("Without stock", best_without_stock)
-        print("With stock", best_with_stock)
-
+        best_with_stock = max(best_with_stock, best_without_stock - x) # buying operation
+        best_without_stock = max(best_without_stock, best_with_stock +x) # selling operation and checking if the operation yields profit
     return best_without_stock
 
 print(maxProfit([7, 1, 5, 3, 6, 4]))
 
+"""
+Briefing
+1) At first we assume the first stock I buy would end up in loss
+2) continue with iteration untill you find a price that would bring a profit
+"""
