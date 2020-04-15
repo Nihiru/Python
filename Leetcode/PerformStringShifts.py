f5cb684572efcb7b9a83cae3c49d5c1fda8efd09
Nihiru <nikhil.m.alors@gmail.com>
2020-04-15 21:26:47 +0530

    Day 12  - Simple Shift (Simple Explanation)

--- Leetcode/PerformStringShifts.py
+++ Leetcode/PerformStringShifts.py
@@ -1,14 +1,19 @@
+"""
+shift[i] = [direction, amount]
+direction can be 0 for left shift or 1 or right shit
+amount is the amount by which string s to be shifted
+"""
 def stringShift(s, shift):
     if not s:
         return ''
-    
+    # traversal of inner lists
     for direction, by in shift:
         by = by % len(s)
         if direction == 0:
-            s = s[by:] + s[:by]
+            s = s[by:] + s[:by] 
         else:
             s = s[-by:] + s[:-by]
-    
+    # Return a new string after performing an operation
     return s
 
 s = "abc"
