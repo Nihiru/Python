f4b6e2f656007145999c97aa80830248c98470be
Nihiru <nikhil.m.alors@gmail.com>
2020-05-07 08:25:12 +0530

    Binary Search Tree in action

--- Leetcode/Trees/BST.py
+++ Leetcode/Trees/BST.py
@@ -8,20 +8,62 @@ class Node:
         self.right = None
         self.val = key
 
-    # an insert operation that specifies 
-    def insert(root, node):
-        if root is None:
-            root = node
-        else:
-            if root.val < node.val:
-                # condition to check if the right node is empty
-                if root.right is None:
-                    root.right = node
-                # pass right node as root
-                else:
-                    insert(root.right, node)
+# an insert operation that specifies 
+def insert(root, node):
+    if root is None:
+        root = node
+    else:
+        if root.val < node.val:
+            # condition to check if the right node is empty
+            if root.right is None:
+                root.right = node
+            # pass right node as root
             else:
-                if root.left is None:
-                    root.left = node
-                else:
-                    insert(root.left, node)
+                insert(root.right, node)
+        else:
+            if root.left is None:
+                root.left = node
+            else:
+                insert(root.left, node)
+
+#  Increasing order
+def inorderTraversalIncreasing(root):
+    if root:
+        inorderTraversalIncreasing(root.left)
+        print(root.val)
+        inorderTraversalIncreasing(root.right)
+
+
+# Decreasing order
+def inorderTraversalDecreasing(root):
+    if root:
+        inorderTraversalDecreasing(root.right)
+        print(root.val)
+        inorderTraversalDecreasing(root.left)
+
+
+def postorderTraversal(root):
+    if root:
+        postorderTraversal(root.left)
+        postorderTraversal(root.right)
+        print(root.val)
+
+
+def preorderTraversal(root):
+    if root:
+        print(root.val)
+        preorderTraversal(root.left)
+        preorderTraversal(root.right)
+
+r = Node(50)
+insert(r, Node(30))
+insert(r, Node(20))
+insert(r, Node(40))
+insert(r, Node(70))
+insert(r, Node(60))
+insert(r, Node(80))
+
+# inorderTraversalIncreasing(r)
+inorderTraversalDecreasing(r)
+# postorderTraversal(r)
+# preorderTraversal(r)
