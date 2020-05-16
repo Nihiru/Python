63af980f106d37a20642809f9260a9fc5728b96e
Nihiru <nikhil.m.alors@gmail.com>
2020-05-16 13:22:51 +0530

    Closure

--- Concepts/Closures.py
+++ Concepts/Closures.py
@@ -1,6 +1,8 @@
 '''
 :Nested functions can access variables of the enclosing scope
 :The technique by which some data gets attached to the code is called closure
+:When there are few methods to be implemented in a class, closures can provide an alternate and more elegant solutions
+:Closure function can access values that are enclosed
 '''
 
 def printer_msg(msg):
@@ -14,3 +16,24 @@ def printer_msg(msg):
 
 closure = printer_msg("hello")
 closure() # call is made to inner function
+
+# beauty
+def make_multiplier_of(n):
+    def multiplier(x):
+        return x * n
+    return multiplier
+
+# Multiplier of 3
+times3 = make_multiplier_of(3)
+
+# Multiplier of 5
+times5 = make_multiplier_of(5)
+
+# Output: 27
+print(times3(9))
+
+# Output: 15
+print(times5(3))
+
+# Output: 30
+print(times5(times3(2)))
