b9e062681978f84c6a3f5bfa560f3b4ec6fee77e
Nihiru <nikhil.m.alors@gmail.com>
2020-06-07 21:07:47 +0530

    Finding the closest value in BST

---
+++ AlgoExpert/ClosestValueInBST.py
@@ -0,0 +1,64 @@
+"""
+:Checking the  closest value in BST
+:Logic
+    :Set the closest value to  -infinity
+    :Compare the current node value with the target value
+        :If current node value is lesser than target value then traverse the right subtree for further comparison
+        :Else traverse left subtree for comparison
+:Complexity:
+    :Recursively
+        :Average Case
+            :Time - O(log(N)) where N is number of nodes in the tree
+            :Space -  O(log(N)) 
+                :Justification - Number of frames created for each recursive call and the memory consumption
+        :Worst Case
+            :Time - O(N)
+                :Justification - Traversing the entire sub-tree when an element is not found
+            :Space - O(N) 
+    
+    :Iteratively
+            :Average Case
+            :Time - O(log(N)) where N is number of nodes in the tree
+            :Space -  O(log(N)) 
+                :Justification - Number of frames created for each recursive call and the memory consumption
+        :Worst Case
+            :Time - O(N)
+                :Justification - Traversing the entire sub-tree when an element is not found
+            :Space - O(N)
+
+"""
+
+
+def findClosestValueInBSTRecursive(tree, target):
+    return findClosestValueInBSTHelperRecursive(tree, target, float("-inf"))
+
+
+def findClosestValueInBSTHelperRecursive(tree, target, closest):
+    if tree is None:
+        return closest
+    if abs(target - closest) > abs(target - tree.value):
+        closest = tree.value
+    if target < tree.value:
+        return findClosestValueInBSTHelperRecursive(tree.left, target, closest)
+    elif target > tree.value:
+        return findClosestValueInBSTHelperRecursive(tree.right, target, closest)
+    else:
+        return closest
+
+
+def findClosestValueInBSTIteratively(tree, target):
+    return findClosestValueInBSTHelperIteratively(tree, target, float("-inf"))
+
+
+def findClosestValueInBSTHelperIteratively(tree, target, closest):
+    currentNode = tree
+    while currentNode is not None:
+        if abs(target - closest) > abs(target - tree.value):
+            closest = tree.value
+        if target < tree.value:
+            currentNode = currentNode.left
+        elif target > tree.value:
+            currentNode = currentNode.right
+        else:
+            break
+    return closest
