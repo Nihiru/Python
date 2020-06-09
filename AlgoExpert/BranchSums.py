d5699bcf700f1832740221ea0fabd7410ec214ba
Nihiru <nikhil.m.alors@gmail.com>
2020-06-09 20:39:29 +0530

    Branch sums - Incomplete

---
+++ AlgoExpert/BranchSums.py
@@ -0,0 +1,13 @@
+"""
+:To find the sum of all the leaf nodes from the root node and return them as a list
+:Logic
+    :
+:Complexity
+    :Time: O(N) 
+        :Reason - Traverse N number of nodes in the tree, constant time operations(calculating the sum with current node \
+                    value) for each node
+    :Space: O(N)
+        :Reason - List of branch sums, Recursive method implementation
+                  
+
+"""
