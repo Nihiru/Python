6870f44943978bd8e5a10e1c95312eb9c0af0eef
Nihiru <nikhil.m.alors@gmail.com>
2020-06-18 21:58:19 +0530

    Linked list

---
+++ AlgoExpert/LinekdListConstruction.py
@@ -0,0 +1,9 @@
+"""
+:Operations
+    :Insert a node at a given position on a linked list, before head, after tail 
+    :Delete a node from a linked list
+    :Search a node in a linked list
+:Logic
+    :Construction of doubly linked list
+"""
+
