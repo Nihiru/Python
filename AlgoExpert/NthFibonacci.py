85eeb483c78b53b25d81059bf3dec0df44cf56cb
Nihiru <nikhil.m.alors@gmail.com>
2020-07-11 09:23:49 +0530

    Manual testing

--- AlgoExpert/NthFibonacci.py
+++ AlgoExpert/NthFibonacci.py
@@ -37,31 +37,38 @@
                 :Compute the first and last from the array and storing it as computer results
 """
 
-#Time: O(2^N) | Space: O(N)
-def getNthFib(n):
+# Time: O(2^N) | Space: O(N)
+def getNthFibApp1(n):
     if n == 2:
         return 1
     elif n == 1:
         return 0
     else:
-        return getNthFib(n - 1) + getNthFib(n - 2)
+        return getNthFibApp1(n - 1) + getNthFibApp1(n - 2)
 
-#Time:O(N)   | Space:O(N) 
-def getNthFib(n, memoize={1: 0, 2: 1}):
+
+# Time:O(N)   | Space:O(N)
+def getNthFibApp2(n, memoize={1: 0, 2: 1}):
     if n in memoize:
         return memoize[n]
     else:
-        memoize[n] = getNthFib(n - 1, memoize) + getNthFib(n - 2, memoize)
+        memoize[n] = getNthFibApp2(n - 1, memoize) + getNthFibApp2(n - 2, memoize)
+        print("Memoize", memoize)
         return memoize[n]
 
 
-#Time:O(N) | Space:O(1)
-def getNthFib(n):
-    lastTwo = [0,1]
+# Time:O(N) | Space:O(1)
+def getNthFibApp3(n):
+    lastTwo = [0, 1]
     counter = 3
     while counter <= n:
         nextFib = lastTwo[0] + lastTwo[1]
         lastTwo[0] = lastTwo[1]
         lastTwo[1] = nextFib
         counter += 1
-    return lastTwo[1] if n > 1 else lastTwo[]
+    # either of these conditions is specified if required fibonacci lies within the existing array
+    return lastTwo[1] if n > 1 else lastTwo[0]
+
+
+# print(getNthFibApp2(4))
+print(getNthFibApp3(2))
